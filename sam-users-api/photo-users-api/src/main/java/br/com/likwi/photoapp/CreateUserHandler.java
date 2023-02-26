0e8e1e1c8fe4a5f1641a9bf974249dbcbd370ee1
Renato Fagalde <renato@likwi.com.br>
2023-02-26 18:40:35 -0300

    createUserService

--- sam-users-api/photo-users-api/src/main/java/br/com/likwi/photoapp/CreateUserHandler.java
+++ sam-users-api/photo-users-api/src/main/java/br/com/likwi/photoapp/CreateUserHandler.java
@@ -1,5 +1,6 @@
 package br.com.likwi.photoapp;
 
+import br.com.likwi.photoapp.service.CognitoUserService;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -8,10 +9,10 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 /**
  * Handler for requests to Lambda function.
@@ -21,36 +22,54 @@ public class CreateUserHandler implements RequestHandler<APIGatewayProxyRequestE
     public static final String APPLICATION_JSON = "application/json";
     public static final String USER_ID = "userId";
 
+    private final String MY_COGNITO_USER_POOL_ID = System.getenv("MY_COGNITO_USER_POOL_ID");
+    private final String MY_COGNITO_CLIENT_APP_SECRET = System.getenv("MY_COGNITO_CLIENT_APP_SECRET");
+//    private final String MY_COGNITO_USER_POOL_ID = DecrtptKey.decryptKey("MY_COGNITO_USER_POOL_ID");
+//    private final String MY_COGNITO_CLIENT_APP_SECRET = DecrtptKey.decryptKey("MY_COGNITO_CLIENT_APP_SECRET");
+
+    private CognitoUserService cognitoUserService;
+
+    public CreateUserHandler() {
+        this.cognitoUserService = new CognitoUserService(System.getenv("AWS_REGION"));
+    }
+
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", APPLICATION_JSON);
-        headers.put("X-Custom-Header", APPLICATION_JSON);
-        LambdaLogger logger = context.getLogger();
-
-        logger.log("Handling CreateUser version: "+context.getFunctionVersion());
-
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-                .withHeaders(headers);
-        String inputBody = input.getBody();
         Gson gson = new Gson();
-
-        logger.log("Original JSON: "+ inputBody);
-        JsonObject userDetails = JsonParser.parseString(inputBody).getAsJsonObject();
-
-
-
-
-        var responseMap = new HashMap();
-        responseMap.put(USER_ID, usersDeteils.get(USER_ID));
-
-
-        HashMap<String, String> responseHeaders = new HashMap<>();
+        LambdaLogger logger = context.getLogger();
+        Gson responseBody = new Gson();
+        Map<String, String> responseHeaders = new HashMap<>();
         responseHeaders.put("Content-Type", APPLICATION_JSON);
+        responseHeaders.put("X-Custom-Header", APPLICATION_JSON);
         responseHeaders.put("Lambda-Version", context.getFunctionVersion());
 
-        return response
+        logger.log("Handling CreateUser version: " + context.getFunctionVersion());
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(responseHeaders)
-                .withStatusCode(200)
-                .withBody(gson.toJson(usersDeteils, Map.class));
+                .withStatusCode(500);
+        String inputBody = input.getBody();
+
+        logger.log("Original JSON: " + inputBody);
+        JsonObject userDetails = JsonParser.parseString(inputBody).getAsJsonObject();
+
+        try{
+
+            JsonObject createUserResult = this.cognitoUserService.createUser(userDetails,
+                    this.MY_COGNITO_USER_POOL_ID,
+                    this.MY_COGNITO_CLIENT_APP_SECRET);
+
+            response.withStatusCode(200);
+            response.withBody(responseBody.toJson(createUserResult, JsonObject.class));
+
+        }catch (AwsServiceException e){
+            logger.log(e.awsErrorDetails().errorMessage());
+            response.withBody(responseBody.toJson(e, AwsServiceException.class));
+        }
+        catch (Exception e){
+            logger.log(e.getLocalizedMessage());
+            response.withBody(responseBody.toJson(e, Exception.class));
+        }
+
+        return response;
     }
 }
